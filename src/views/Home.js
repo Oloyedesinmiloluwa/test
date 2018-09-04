d9c54c51aa0be0d99d5e9fd9f70d2815a001089a
Veeqtor <nwokeochavictor@gmail.com>
2018-09-04 09:27:06 +0100

    ft(HeaderCard): Create the header card components
    - Write unit tests
    - Add semantic-ui CDN
    - Add semanetic-ui-react
    - Create the header card component
    
    [Delivers #160238582]

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,6 +3,9 @@ import { user } from '../tests/__mocks__/mockData';
 import Avatar from '../components/Avatar';
 import LoginCard from '../components/LoginCard';
 import HeaderCard from '../components/HeaderCard';
+import { Grid } from 'semantic-ui-react';
+import HeaderCard from '../components/HeaderCard';
+import articles from '../tests/__mocks__/mockData';
 
 const Home = () => (
   <div>
@@ -10,6 +13,11 @@ const Home = () => (
     <Avatar user={user} />
     <HeaderCard />
     <LoginCard />
+
+    {/* Replace the articles prop with the response from the get articles request call  */}
+    <Grid id="header-card" stackable>
+      <HeaderCard articles={articles.articles} />
+    </Grid>
   </div>
 );
 
